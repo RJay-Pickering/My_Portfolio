e20e4058b09f2c7cac4af51ee1103ecd5530badc
RJay-Pickering <rjaypickering@gmail.com>
2024-04-16 09:25:43 -0500

    trying to do .gitignore stuff

--- js/secret.js
+++ js/secret.js
@@ -5,37 +5,24 @@ const months = ["January","February","March","April","May","June","July","August
 
 const dates = new Date();
 let current_date = months[dates.getMonth()];
-// end of getting month
+let current_time = dates.getDate();
+// end of getting month and day
 
 season = document.getElementById("season")
 season.src = "https://c.tenor.com/Xh8ndEFOCJcAAAAM/dancing-grooving.gif"
 season.style.display = "none"
 
-if (current_date == "October") {
-  // secret image inside my portfolio 🤫
-  season.style.display = null
-  season.addEventListener("click", () => {
-    console.log("Achievement Unlocked: Find hidden dancing bones")
-    season.classList.toggle("season_gif")
-    if (season.classList != "season_gif") {
-      season.style.width = "10vh";
-      season.style.height = "10vh";
-    } else {
-      season.style.width = null;
-      season.style.height = null;
-    }
-  })
-  // end of secret image
-
+function inactive_secret_image(image_url, sound, message, size) {
   // an inactivity section for something to happen
   var inactive_img = document.getElementById("inactive_id");
-  inactive_img.src = "imageStorage/seasons/skeleton.png"
+  inactive_img.src = image_url
   inactive_img.style.display = "none";
-  var nowPlaying = "sounds/halloweenBackgroundMusic.mp3" ;
+  inactive_img.style.width = size
+  var nowPlaying = sound;
   var audio = new Audio(nowPlaying);
 
-  onInactive(65000, function () {
-    console.log("Achievement Unlocked: You died while viewing this website...")
+  onInactive(5000, function () {
+    console.log(message)
     inactive_img.style.display = null;
     audio.autoplay = true;
     audio.loop = true;
@@ -55,36 +42,47 @@ if (current_date == "October") {
     };
   }
   // end of inactivity
-} else if (current_date == "November") {
-  // an inactivity section for something to happen
-  var inactive_img = document.getElementById("inactive_id");
-  inactive_img.src = "imageStorage/seasons/charlie-brown-thanksgiving.jpeg"
-  inactive_img.style.display = "none";
-  inactive_img.style.width = "100%"
-  var nowPlaying = "sounds/halloweenBackgroundMusic.mp3" ;
-  var audio = new Audio(nowPlaying);
+}
 
-  onInactive(65000, function () {
-    inactive_img.style.display = null;
-    audio.autoplay = true;
-    audio.loop = true;
-    audio.load();
-    audio.play();
-  });
-
-  function onInactive(ms, cb) {
-
-    var wait = setTimeout(cb, ms);
-
-    document.onmousemove = document.mousedown = document.mouseup = document.onkeydown = document.onkeyup = document.focus = function () {
-        clearTimeout(wait);
-        wait = setTimeout(cb, ms);
-        inactive_img.style.display = "none";
-        audio.pause();
-    };
-  }
-  // end of inactivity
+if (current_date == "January" && current_time == 1) {
+  const achievementMessage = "Achievement Unlocked: You visited me on New Years day? Put down to potato chips and spend your time with your family, you weirdo!"
+  inactive_secret_image("imageStorage/seasons/newyears.png", "none", achievementMessage, "initial")
+} else if (current_date == "February" && current_time == 14) {
+  const achievementMessage = "Achievement Unlocked: If your seeing this now, you obviously get no bitches! Your a bitchless weirdo!"
+  inactive_secret_image("imageStorage/seasons/valentines.png", "none", achievementMessage, "10em")
+} else if (current_date == "March" && current_time == 17) {
+    const achievementMessage = "Achievement Unlocked: Its saint patty's day, what are you doing here? Go to the pub you bloody weirdo!"
+    inactive_secret_image("imageStorage/seasons/stpattysday.png", "none", achievementMessage, "10em")
+} else if (current_date == "July" && current_time == 4) {
+    const achievementMessage = "Achievement Unlocked: Its 4th of July, go shoot fireworks weirdo!"
+    inactive_secret_image("imageStorage/seasons/fireworks.gif", "none", achievementMessage, "initial")
 } else {
   var inactive_img = document.getElementById("inactive_id");
   inactive_img.style.display = "none";
 }
+
+
+// if (current_date == "October") {
+//   // secret image inside my portfolio 🤫
+//   season.style.display = null
+//   season.addEventListener("click", () => {
+//     console.log("Achievement Unlocked: Find hidden dancing bones")
+//     season.classList.toggle("season_gif")
+//     if (season.classList != "season_gif") {
+//       season.style.width = "10vh";
+//       season.style.height = "10vh";
+//     } else {
+//       season.style.width = null;
+//       season.style.height = null;
+//     }
+//   })
+//   // end of secret image
+//   const achievementMessage = "Achievement Unlocked: You died while viewing this website..."
+//   inactive_secret_image("imageStorage/seasons/skeleton.png", "sounds/halloweenBackgroundMusic.mp3", achievementMessage, "initial")
+// } else if (current_date == "November") {
+//   const achievementMessage = "Achievement Unlocked: Happy Thanksgiving weirdo =)"
+//   inactive_secret_image("imageStorage/seasons/thanksgiving.jpeg", "none", achievementMessage, "90em")
+// } else if (current_date == "December") {
+//   const achievementMessage = "Achievement Unlocked: You saw santa delivering presents to you... Merry Christmas Weirdo =)"
+//   inactive_secret_image("imageStorage/seasons/santa.png", "none", achievementMessage, "initial")
+// }
